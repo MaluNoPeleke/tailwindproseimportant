f79dcc8dd5537b4f53fb4b1010f2b570e3a82cd2
Frederik Niedernolte <f.niedernolte@gmail.com>
2021-09-14 14:13:34 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    mode: 'jit',
+    purge: {
+        preserveHtmlElements: true,
+        mode: 'layers',
+        enabled: true,
+        content: [
+            './site/templates/*.php',
+            './site/snippets/*.php',
+            './site/postcss/*.css',
+        ],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        pre: null,
+                        code: null,
+                        'code::before': null,
+                        'code::after': null,
+                        'pre code': null,
+                        'pre code::before': null,
+                        'pre code::after': null,
+                    },
+                },
+             },
+        },
+    },
+    variants: {
+        extend: {},
+        display: ["group-hover"],
+        transform: ['hover', 'focus'],
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
